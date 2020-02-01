aaa4a5fe29af777ff219fc1b6a26b8a13d5ca6ab
Daniel Murth <daniel.murth@chello.at>
2020-02-01 11:19:27 +0100

    change tabs to spaces ... just formatting stuff

--- includes/scripts.php
+++ includes/scripts.php
@@ -18,20 +18,20 @@ if (!defined('ABSPATH')) {
 
 // load backend styles and scripts
 function load_backend_scripts() {
-	wp_register_style('custom_backend_css', plugins_url( '../admin/css/backend.css', __FILE__));
-	wp_enqueue_style('custom_backend_css');
+  wp_register_style('custom_backend_css', plugins_url( '../admin/css/backend.css', __FILE__));
+  wp_enqueue_style('custom_backend_css');
 
-	wp_register_script('custom_backend_js', plugins_url( '../admin/js/backend.js', __FILE__ ));
-	wp_enqueue_script('custom_backend_js');
+  wp_register_script('custom_backend_js', plugins_url( '../admin/js/backend.js', __FILE__ ));
+  wp_enqueue_script('custom_backend_js');
 }
 add_action('admin_enqueue_scripts', 'load_backend_scripts');
 
 // load frontend styles and scripts
 function load_frontend_scripts() {
-	wp_register_style('custom_frontend_css', plugins_url( '../public/css/styles.css', __FILE__));
-	wp_enqueue_style('custom_frontend_css');
+  wp_register_style('custom_frontend_css', plugins_url( '../public/css/styles.css', __FILE__));
+  wp_enqueue_style('custom_frontend_css');
 
-	wp_register_script( 'custom_frontend_js', plugins_url( '../public/js/main.js', __FILE__ ));
+  wp_register_script( 'custom_frontend_js', plugins_url( '../public/js/main.js', __FILE__ ));
   wp_enqueue_script('custom_frontend_js');
 }
 add_action('wp_enqueue_scripts', 'load_frontend_scripts');
